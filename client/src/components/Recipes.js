e64422abbb85ef4d9890b518369bcad45ff9c2ce
cmstexas <crystal.m.soria@gmail.com>
2019-08-09 16:29:13 -0500

    edits

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -38,22 +38,3 @@ export default Recipes;
 
 
 
-
-
-// const Recipes = (props) => {
-//     return (
-//     <div>
-//        {users.map((user => ( 
-//             <div>
-//                 <h2>Recipes</h2>
-//                 <h3>{user.name}</h3>
-//                 <h3>{user.course}</h3>
-//                 <h4>{user.technique}</h4>
-//                 <p>{user.ingredients}</p>
-//             </div>   
-//             )))} 
-//     </div>
-//      )}
-
-
-// export default Recipes;
